867e21e6fa93aa152d87091f56b1788ccfb5d6ff
03-lovepreetSingh <lovepreetsingh9810573475@gmail.com>
2024-04-28 04:12:29 +0530

    4

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,13 @@ import cors from "cors";
 const app = express();
 dotenv.config();
 const connect = () => {
-  mongoose.connect(process.env.MONGO).then(() => {
-    console.log("Connected to Database");
-  });
+  mongoose
+    .connect(
+      "mongodb+srv://love:love@cluster0.8cz0ggr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => {
+      console.log("Connected to Database");
+    });
 };
 app.use(
   cors({
